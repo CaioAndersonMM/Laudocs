7eefd83891993e9089c434783af140897b199422
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-10-26 14:46:24 -0300

    update: Correção das alturas dos containers

--- laudocs/src/app/page.tsx
+++ laudocs/src/app/page.tsx
@@ -1,31 +1,29 @@
-// app/page.tsx
-import CardPatient from '../components/CardPatient';
 import SignUp from '@/components/SigUp';
 import React from 'react';
 import ListPatients from '@/components/ListPatients';
 import { CardPatientInterface } from '@/interfaces/CardPatientInterface';
 
 const patients: CardPatientInterface[] = [
-  { id: 1, name: 'João', age: 23, solicitingDoctor: 'Dr. Silva' },
-  { id: 2, name: 'Maria', age: 45, solicitingDoctor: 'Dr. Santos' },
-  { id: 3, name: 'José', age: 33, solicitingDoctor: 'Dr. Oliveira' },
-  { id: 4, name: 'Ana', age: 19, solicitingDoctor: 'Dr. Costa' },
-  { id: 5, name: 'Carlos', age: 67, solicitingDoctor: 'Dr. Pereira' },
-  { id: 6, name: 'Paula', age: 25, solicitingDoctor: 'Dr. Almeida' },
-  { id: 7, name: 'Pedro', age: 50, solicitingDoctor: 'Dr. Ferreira' },
-  { id: 8, name: 'Mariana', age: 37, solicitingDoctor: 'Dr. Lima' },
+  { id: 1, name: 'João Paulo da Silva Souza', age: 23, solicitingDoctor: 'Dr. Silva' },
+  { id: 2, name: 'Maria da Costa Silva Pereira', age: 45, solicitingDoctor: 'Dr. Santos' },
+  { id: 3, name: 'José Armando de Souza', age: 33, solicitingDoctor: 'Dr. Oliveira' },
+  { id: 4, name: 'Ana Maria dos Santos', age: 19, solicitingDoctor: 'Dr. Costa' },
+  { id: 5, name: 'Carlos Alberto Vieira Costa', age: 67, solicitingDoctor: 'Dr. Pereira' },
+  { id: 6, name: 'Paula Eliza Martins dos Santos', age: 25, solicitingDoctor: 'Dr. Almeida' },
+  { id: 7, name: 'Pedro Pereira Santiago Filho', age: 50, solicitingDoctor: 'Dr. Ferreira' },
+  { id: 8, name: 'Mariana Karla Pinheiro', age: 37, solicitingDoctor: 'Dr. Lima' },
 ];
 
 export default function Home() {
   return (
-    <div className="bg-gray-100 h-screen">
+    <div className="bg-gray-100 h-screen p-1">
       <div className="grid grid-cols-1 md:grid-cols-2 p-5 items-stretch h-full md:gap-0 gap-x-4">
-      
-        <div>
-          <ListPatients arrayOfPatients = {patients} />
+
+        <div className="flex-1">
+          <ListPatients arrayOfPatients={patients} />
         </div>
- 
-        <div className="flex h-full">
+
+        <div className="flex h-[97%]">
           <SignUp />
         </div>
 
