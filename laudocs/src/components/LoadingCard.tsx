173fa6332a57ec5bad39cbe2a4675066e0d726b5
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-11-01 17:40:05 -0300

    feat: Adiciona componente LoadingCard para melhorar a exibição dos CARDS durante o carregamento

---
+++ laudocs/src/components/LoadingCard.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const LoadingCard = () => {
+  return (
+    <div className="bg-white shadow-md rounded-lg p-2 mb-3 animate-pulse">
+      <div className="flex items-center justify-between mb-1">
+        <div className="flex items-center ml-2">
+          <div className="h-8 w-8 bg-gray-300 rounded-full mr-2"></div>
+          <div className="h-4 bg-gray-300 rounded w-3/4 mb-1"></div>
+        </div>
+        <div className="h-6 w-16 bg-gray-300 rounded"></div>
+      </div>
+      <div className="flex items-center justify-between mb-1">
+        <div className="flex items-center ml-2">
+          <div className="h-8 w-8 bg-gray-300 rounded-full mr-2"></div>
+          <div className="h-4 bg-gray-300 rounded w-1/2"></div>
+        </div>
+        <div className="h-6 w-16 bg-gray-300 rounded"></div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingCard;
