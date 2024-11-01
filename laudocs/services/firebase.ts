5027a6a732a6f809ddd8d444926ac346621696f9
BEN ARIEL FRANÇA MARTINS <fadomarshemello@gmail.com>
2024-10-31 21:21:33 -0300

    protegendo dados de configuração do banco de dados

--- laudocs/services/firebase.ts
+++ laudocs/services/firebase.ts
@@ -2,18 +2,16 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCx80ja8AYou8KICz6Gn2yBx_2sP3jCY64",
-  authDomain: "laudocs-b4226.firebaseapp.com",
-  databaseURL: "https://laudocs-b4226-default-rtdb.firebaseio.com",
-  projectId: "laudocs-b4226",
-  storageBucket: "laudocs-b4226.appspot.com",
-  messagingSenderId: "1028493008939",
-  appId: "1:1028493008939:web:1a1236e572c62ceec20a6e"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
-
-
 const database = getFirestore(app);
 
 export { database, app };
