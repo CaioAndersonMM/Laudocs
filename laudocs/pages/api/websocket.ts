43e795fb5cf223021236efa3ee3e3ea4c336cc34
BEN ARIEL FRANÇA MARTINS <fadomarshemello@gmail.com>
2024-10-31 21:20:44 -0300

    exclusão de arquivo desnecessário

--- laudocs/pages/api/websocket.ts
+++
@@ -1,9 +0,0 @@
-import { WebSocketServer } from 'ws';
-
-const wss = new WebSocketServer({ port: 3000 });
-
-wss.on('connection', function connection(ws) {
-ws.on('message', function incoming(message) {
-ws.send(`Server received: ${message}`);
-});
-});
