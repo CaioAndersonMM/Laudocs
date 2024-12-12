5ba18af2a710c252a05e7115ae9be3f6b63dd40f
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-12-12 14:10:35 -0300

    feat: Adicionado renderização de ambos formulário para informações dos nódulos e carregamento estático dos selects

--- laudocs/src/components/typeultrassom/FormUltrassom.tsx
+++ laudocs/src/components/typeultrassom/FormUltrassom.tsx
@@ -12,15 +12,15 @@ interface FormUltrassomProps {
 }
 
 type FormState = {
-    [key: string]: string | boolean;
+    [key: string]: string | boolean | FormState;
 };
 
 const FormUltrassom = ({ tipo, patientName, patientAge, solicitingDoctor }: FormUltrassomProps) => {
-
     const [data, setData] = useState(new Date().toISOString().split('T')[0]);
     const [formState, setFormState] = useState<FormState>({});
     const [hasNodule, setHasNodule] = useState(false);
     const [hasLinfonodo, setHasLinfonodo] = useState(false);
+    const [noduleLocation, setNoduleLocation] = useState<string>('Esquerda');
 
     const formQuestions = Questions[tipo] || { Selects: [], Checkbox: [] };
 
@@ -31,9 +31,20 @@ const FormUltrassom = ({ tipo, patientName, patientAge, solicitingDoctor }: Form
             initialFormState[question.label] = question.options[0] || "";
         });
 
+        formQuestions.Checkbox.forEach((question) => {
+            initialFormState[question.label] = false;
+        });
+
+        noduleQuestions.forEach((question) => {
+            initialFormState[`esquerda_${question.mark}`] = question.options[0] || "";
+            initialFormState[`direita_${question.mark}`] = question.options[0] || "";
+        });
+
         setFormState((prevState) => ({
             ...prevState,
             ...initialFormState,
+            'Tem nódulo?': 'Não',
+            'Onde está o Nódulo?': 'Esquerda'
         }));
     }, [formQuestions]);
 
@@ -50,35 +61,55 @@ const FormUltrassom = ({ tipo, patientName, patientAge, solicitingDoctor }: Form
         if (questionLabel === 'Linfonodos axilares têm aspecto não habitual?') {
             setHasLinfonodo(value === 'Sim');
         }
+
+        if (questionLabel === 'Onde está o Nódulo?') {
+            setNoduleLocation(value);
+        }
+    };
+
+    const handleInputChange = (questionLabel: string, value: string) => {
+        setFormState((prevState) => ({
+            ...prevState,
+            [questionLabel]: value
+        }));
+    };
+
+    const handleCheckboxChange = (questionLabel: string, checked: boolean) => {
+        setFormState((prevState) => ({
+            ...prevState,
+            [questionLabel]: checked
+        }));
     };
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
+        console.log('Form State:', formState);
+
         const substituicoes = preencherSubstituicoes(formState, tipo, patientName, patientAge, data, solicitingDoctor);
 
         console.log(JSON.stringify(substituicoes));
 
-        // try {
-        //     const response = await fetch('/api/gerar-doc', {
-        //         method: 'POST',
-        //         headers: {
-        //             'Content-Type': 'application/json',
-        //         },
-        //         body: JSON.stringify({ substituicoes })
-        //     });
+        try {
+            const response = await fetch('/api/gerar-doc', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ substituicoes })
+            });
 
-        //     if (!response.ok) {
-        //         const errorData = await response.json();
-        //         throw new Error(`Erro: ${errorData.message}`);
-        //     }
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(`Erro: ${errorData.message}`);
+            }
 
-        //     const data = await response.json();
-        //     alert(data.message);
-        // } catch (error) {
-        //     console.error("Erro ao enviar o formulário:", error);
-        //     alert("Erro ao gerar o documento: " + (error as Error).message);
-        // }
+            const data = await response.json();
+            alert(data.message);
+        } catch (error) {
+            console.error("Erro ao enviar o formulário:", error);
+            alert("Erro ao gerar o documento: " + (error as Error).message);
+        }
     };
 
     return (
@@ -100,7 +131,6 @@ const FormUltrassom = ({ tipo, patientName, patientAge, solicitingDoctor }: Form
                                     onChange={(e) => handleSelectChange(question.label, e.target.value)}
                                     className="ml-2 p-2 border rounded-md"
                                 >
-
                                     {question.options.map((option, idx) => (
                                         <option key={idx} value={option}>{option}</option>
                                     ))}
@@ -119,34 +149,73 @@ const FormUltrassom = ({ tipo, patientName, patientAge, solicitingDoctor }: Form
                             </h1>
                         </div>
 
-                        <div className="grid grid-cols-4 gap-4">
-                            {noduleQuestions.map((question, index) => (
-                                <div key={index} className="mb-4 p-4 bg-gray-100 rounded-lg shadow-md hover:bg-gray-200 transition-all duration-300 ease-in-out">
-                                    <label className="block text-lg font-semibold text-gray-700">
-                                        {question.label}
-                                        {question.isNumberInput ? (
-                                            <input
-                                                type="number"
-                                                value={formState[question.label] as string || ""}
-                                                onChange={(e) => handleSelectChange(question.label, e.target.value)}
-                                                className="ml-2 p-2 border rounded-md w-full bg-white focus:ring-2 focus:ring-cyan-800"
-                                                placeholder="Digite a medida"
-                                            />
-                                        ) : (
-                                            <select
-                                                value={formState[question.label] as string || question.options[0]}
-                                                onChange={(e) => handleSelectChange(question.label, e.target.value)}
-                                                className="ml-1 border rounded-md w-full p-2 bg-white focus:ring-2 focus:ring-cyan-800"
-                                            >
-                                                {question.options.map((option, idx) => (
-                                                    <option key={idx} value={option}>{option}</option>
-                                                ))}
-                                            </select>
-                                        )}
-                                    </label>
+                        {(noduleLocation === 'Esquerda' || noduleLocation === 'Ambas') && (
+                            <>
+                                <h2 className="mt-6 text-xl font-semibold">Informações sobre o nódulo na posição Esquerda</h2>
+                                <div className="grid grid-cols-4 gap-4 mt-4">
+                                    {noduleQuestions.map((question, index) => (
+                                        <div key={index} className="mb-4 p-4 bg-gray-100 rounded-lg shadow-md hover:bg-gray-200 transition-all duration-300 ease-in-out">
+                                            <label className="block text-lg font-semibold text-gray-700">
+                                                {question.label}
+                                                {question.isNumberInput ? (
+                                                    <input
+                                                        type="number"
+                                                        value={formState[`esquerda_${question.mark}`] as string || ""}
+                                                        onChange={(e) => handleInputChange(`esquerda_${question.mark}`, e.target.value)}
+                                                        className="ml-2 p-2 border rounded-md w-full bg-white focus:ring-2 focus:ring-cyan-800"
+                                                        placeholder="Digite a medida"
+                                                    />
+                                                ) : (
+                                                    <select
+                                                        value={formState[`esquerda_${question.mark}`] as string || question.options[0]}
+                                                        onChange={(e) => handleInputChange(`esquerda_${question.mark}`, e.target.value)}
+                                                        className="ml-1 border rounded-md w-full p-2 bg-white focus:ring-2 focus:ring-cyan-800"
+                                                    >
+                                                        {question.options.map((option, idx) => (
+                                                            <option key={idx} value={option}>{option}</option>
+                                                        ))}
+                                                    </select>
+                                                )}
+                                            </label>
+                                        </div>
+                                    ))}
                                 </div>
-                            ))}
-                        </div>
+                            </>
+                        )}
+
+                        {(noduleLocation === 'Direita' || noduleLocation === 'Ambas') && (
+                            <>
+                                <h2 className="mt-6 text-xl font-semibold">Informações sobre o nódulo na posição Direita</h2>
+                                <div className="grid grid-cols-4 gap-4 mt-4">
+                                    {noduleQuestions.map((question, index) => (
+                                        <div key={index} className="mb-4 p-4 bg-gray-100 rounded-lg shadow-md hover:bg-gray-200 transition-all duration-300 ease-in-out">
+                                            <label className="block text-lg font-semibold text-gray-700">
+                                                {question.label}
+                                                {question.isNumberInput ? (
+                                                    <input
+                                                        type="number"
+                                                        value={formState[`direita_${question.mark}`] as string || ""}
+                                                        onChange={(e) => handleInputChange(`direita_${question.mark}`, e.target.value)}
+                                                        className="ml-2 p-2 border rounded-md w-full bg-white focus:ring-2 focus:ring-cyan-800"
+                                                        placeholder="Digite a medida"
+                                                    />
+                                                ) : (
+                                                    <select
+                                                        value={formState[`direita_${question.mark}`] as string || question.options[0]}
+                                                        onChange={(e) => handleInputChange(`direita_${question.mark}`, e.target.value)}
+                                                        className="ml-1 border rounded-md w-full p-2 bg-white focus:ring-2 focus:ring-cyan-800"
+                                                    >
+                                                        {question.options.map((option, idx) => (
+                                                            <option key={idx} value={option}>{option}</option>
+                                                        ))}
+                                                    </select>
+                                                )}
+                                            </label>
+                                        </div>
+                                    ))}
+                                </div>
+                            </>
+                        )}
                     </div>
                 )}
 
@@ -156,10 +225,7 @@ const FormUltrassom = ({ tipo, patientName, patientAge, solicitingDoctor }: Form
                             <input
                                 type="checkbox"
                                 checked={formState[question.label] as boolean || false}
-                                onChange={(e) => setFormState((prevState) => ({
-                                    ...prevState,
-                                    [question.label]: e.target.checked
-                                }))}
+                                onChange={(e) => handleCheckboxChange(question.label, e.target.checked)}
                                 className="mr-2"
                             />
                             {question.label}
