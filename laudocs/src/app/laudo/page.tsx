2e71f9d9d010876f76a6882150818ad95e8a7dca
CaioAndersonMM <caioandersonmartins@gmail.com>
2025-01-18 14:03:45 -0300

    feat: Adiciona lógica para tratamento de nódulos e Doppler no estado do formulário, melhorando a exibição dos resultados

--- laudocs/src/app/laudo/page.tsx
+++ laudocs/src/app/laudo/page.tsx
@@ -12,9 +12,52 @@ const Laudo = () => {
         return new Date(dateString).toLocaleDateString('pt-BR', options);
     };
 
-    const parsedFormState = formState ? JSON.parse(formState as string) : {};
+    let parsedFormState = formState ? JSON.parse(formState as string) : {};
 
-    console.log(parsedFormState);
+    console.log(parsedFormState.noduleData.esquerda);
+
+    if (parsedFormState['Tem nódulo?'] === 'Sim') {
+        delete parsedFormState['Tem nódulo?'];
+
+        if (parsedFormState['Onde está o Nódulo?'] === 'Esquerda') {
+            parsedFormState['Nódulo esquerdo'] = 'Presente';
+        } else if (parsedFormState['Onde está o Nódulo?'] === 'Direita') {
+            parsedFormState['Nódulo direito'] = 'Presente';
+        } else if (parsedFormState['Onde está o Nódulo?'] === 'Ambos') {
+            parsedFormState['Nódulo esquerdo'] = 'Presente';
+            parsedFormState['Nódulo direito'] = 'Presente';
+        }
+
+        parsedFormState['Presença de nódulo'] = 'Sim';
+        delete parsedFormState['Onde está o Nódulo?'];
+    } else if (parsedFormState['Tem nódulo?'] === 'Não') {
+        parsedFormState['Nenhum nódulo encontrado'] = 'Sim';
+        delete parsedFormState['Tem nódulo?'];
+    }
+
+    if (parsedFormState['Há Doppler?'] === 'Sim') {
+        parsedFormState['Doppler presente'] = 'Sim';
+        delete parsedFormState['Há Doppler?'];
+    }
+
+    const idadePaciente = parsedFormState.patientAge;
+    delete parsedFormState.patientAge;
+
+    const renderField = (key: string, value: string) => {
+        let label = key;
+        if (value === 'Com' || value === 'Sem') {
+            label = `${value} ${key}`;
+        } else if (value === 'Comprometido' || value === 'Não comprometido') {
+            label = `${key} ${value}`;
+        }
+        return (
+            <div key={key} className="flex items-center space-x-2">
+                <span className="block text-lg font-semibold text-cyan-900 truncate" title={label}>
+                    - {label}
+                </span>
+            </div>
+        );
+    };
 
     return (
         <div className="max-w-4xl mx-auto p-8 bg-white shadow-lg rounded-lg">
@@ -34,7 +77,7 @@ const Laudo = () => {
                     </div>
                     <div>
                         <label className="block text-lg font-semibold text-cyan-900 opacity-55">Idade do Paciente</label>
-                        <p className="mt-2 p-2 border rounded-md bg-gray-100 text-cyan-900 font-semibold capitalize">{parsedFormState.patientAge}</p>
+                        <p className="mt-2 p-2 border rounded-md bg-gray-100 text-cyan-900 font-semibold capitalize">{idadePaciente}</p>
                     </div>
                     <div>
                         <label className="block text-lg font-semibold text-cyan-900 opacity-55">Médico Solicitante</label>
@@ -42,46 +85,52 @@ const Laudo = () => {
                     </div>
                     <div>
                         <label className="block text-lg font-semibold text-cyan-900 opacity-55">Data</label>
-                        <p className="mt-2 p-2 border rounded-md bg-gray-100 text-cyan-900 font-semibold">{formatDate(parsedFormState.data)}
-                        </p>
+                        <p className="mt-2 p-2 border rounded-md bg-gray-100 text-cyan-900 font-semibold">{formatDate(parsedFormState.data)}</p>
                     </div>
                 </div>
             </section>
-
             <section className="mb-8">
                 <h2 className="text-2xl font-semibold mb-4 text-cyan-900 opacity-70">Resultados do Ultrassom</h2>
                 <div className="space-y-4">
                     {Object.entries(parsedFormState)
-                        .filter(([key]) => !['tipo', 'patientName', 'patientAge', 'solicitingDoctor', 'data'].includes(key))
-                        .reduce((acc: [string, string][][], [key, value], index) => {
-                            if (index % 2 === 0) acc.push([[key, value as string]]);
-                            else acc[acc.length - 1].push([key, value as string]);
-                            return acc;
-                        }, [])
-                        .map((pairGroup, index) => (
-                            <div key={index} className="grid grid-cols-2 gap-4">
-                                {pairGroup.map(([key, value]) => (
-                                    <div key={key} className="flex flex-col">
-                                        <label
-                                            className="block text-lg font-semibold text-cyan-900 truncate"
-                                            title={key} // Tooltip com o valor completo da label
-                                        >
-                                            {key}
-                                        </label>
-                                        <p
-                                            className="mt-2 p-2 border rounded-md bg-cyan-900 opacity-80 font-bold truncate"
-                                            title={String(value)} // Tooltip com o valor completo
-                                        >
-                                            {String(value)}
-                                        </p>
-                                    </div>
-                                ))}
-                            </div>
-                        ))}
+                        .filter(([key, value]) => value === 'Sim')
+                        .map(([key, value]) => renderField(key, value as string))}
                 </div>
+                {parsedFormState.noduleData && (
+                    <div className="grid grid-cols-2 gap-4 mt-4">
+                        {parsedFormState.noduleData.esquerda && Object.keys(parsedFormState.noduleData.esquerda).length > 0 && (
+                            <div>
+                                <h3 className="text-xl font-semibold text-bl bg-cyan-700 p-2 rounded max-w-xs mb-3">Nódulo esquerdo</h3>
+                                <div className="space-y-2">
+                                    {Object.entries(parsedFormState.noduleData.esquerda).map(([key, value]) => renderField(key, value as string))}
+                                </div>
+                            </div>
+                        )}
+                        {parsedFormState.noduleData.direita && Object.keys(parsedFormState.noduleData.direita).length > 0 && (
+                            <div>
+                                <h3 className="text-xl font-semibold text-bl bg-cyan-700 p-2 rounded max-w-xs">Nódulo direito</h3>
+                                <div className="space-y-2">
+                                    {Object.entries(parsedFormState.noduleData.direita).map(([key, value]) => renderField(key, value as string))}
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                )}
+
+                {parsedFormState.dopplerData && (
+                    <div className="grid grid-cols-2 gap-4 mt-4">
+                        {Object.keys(parsedFormState.dopplerData).length > 0 && (
+                            <div>
+                                <h3 className="text-xl font-semibold text-bl bg-cyan-700 p-2 rounded max-w-xs mb-3">Doppler</h3>
+                                <div className="space-y-2">
+                                    {Object.entries(parsedFormState.dopplerData).map(([key, value]) => renderField(key, value as string))}
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                )}
             </section>
 
-            
             <div className="mt-24 text-center">
                 <hr className="border-t border-gray-900 mb-4 w-1/2 mx-auto" />
                 <p className="text-lg font-semibold text-gray-500">Dr. MAURO P. F. DE CARVALHO JÚNIOR</p>
