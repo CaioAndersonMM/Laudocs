1614a0f18afa3fa635a27387b8f28663a66a2a8d
CaioAndersonMM <caioandersonmartins@gmail.com>
2025-01-28 03:28:29 -0300

    feat: Adicionada funcionalidade para campos condicionais com suporte a entradas numéricas e melhorias na estrutura de layout

--- laudocs/src/components/typeultrassom/ConditionalField.tsx
+++ laudocs/src/components/typeultrassom/ConditionalField.tsx
@@ -18,13 +18,13 @@ const ConditionalSection: React.FC<ConditionalSectionProps> = ({
     handleConditionalChange
 }) => {
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col space-y-4">
             <label className="block text-sm font-semibold">
                 {select.label}
                 <select
                     value={formState[select.mark] as string || select.options[0]}
                     onChange={(e) => handleSelectChange(select.mark, e.target.value)}
-                    className="p-2 border rounded-md w-full bg-white focus:ring-2 focus:ring-cyan-800"
+                    className="mt-1 p-2 border rounded-md w-full bg-white focus:ring-2 focus:ring-cyan-800"
                 >
                     {select.options.map((option: string, idx: number) => (
                         <option key={idx} value={option}>{option}</option>
@@ -34,22 +34,33 @@ const ConditionalSection: React.FC<ConditionalSectionProps> = ({
             {isConditionMet && conditionField && (
                 <div className="mt-2">
                     <h2 className="text-lg font-semibold bg-cyan-900 text-white p-1 rounded text-center">Informações sobre {select.label}</h2>
-                    <div className="grid grid-cols-3 gap-4 mb-4 p-4 bg-gray-100 rounded-lg shadow-md hover:bg-gray-200 transition-all duration-300 ease-in-out">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-4 p-4 bg-gray-100 rounded-lg shadow-md hover:bg-gray-200 transition-all duration-300 ease-in-out">
                         {conditionField.fields.map((field: any, index: number) => (
                             <div key={index} className="flex flex-col">
-                                <label className="block text- font-semibold">
+                                <label className="block text-sm font-semibold">
                                     {field.label}
-                                    <select
-                                        value={typeof formState.conditionalData[select.mark]?.fields[field.label] === 'boolean'
-                                            ? (formState.conditionalData[select.mark]?.fields[field.label] ? 'Sim' : 'Não')
-                                            : formState.conditionalData[select.mark]?.fields[field.label] || field.options[0]}
-                                        onChange={(e) => handleConditionalChange(select.mark, field.label, e.target.value)}
-                                        className="mt-2 p-2 border rounded-md w-full bg-white focus:ring-2 focus:ring-cyan-800"
-                                    >
-                                        {field.options.map((option: string, idx: number) => (
-                                            <option key={idx} value={option}>{option}</option>
-                                        ))}
-                                    </select>
+                                    {field.isNumberInput ? (
+                                        <input
+                                            type="number"
+                                            // value={formState.condicionalData[field.mark] || ""}
+                                            onChange={(e) => handleConditionalChange(select.mark, field.label, e.target.value)}
+                                            className="mt-2 p-2 border rounded-md w-full bg-white focus:ring-2 focus:ring-cyan-800"
+                                            placeholder="Digite a medida"
+                                        />
+                                    ) : (
+                                        <select
+                                            value={typeof formState.conditionalData[select.mark]?.fields[field.label] === 'boolean'
+                                                ? (formState.conditionalData[select.mark]?.fields[field.label] ? 'Sim' : 'Não')
+                                                : formState.conditionalData[select.mark]?.fields[field.label] || field.options[0]}
+                                            onChange={(e) => handleConditionalChange(select.mark, field.label, e.target.value)}
+                                            className="mt-1 p-2 border rounded-md w-full bg-white focus:ring-2 focus:ring-cyan-800"
+                                        >
+                                            {field.options.map((option: string, idx: number) => (
+                                                <option key={idx} value={option}>{option}</option>
+                                            ))}
+                                        </select>
+                                    )}
+
                                 </label>
                             </div>
                         ))}
