cff6ceacf7f27e6e5d16edd11a57893459016a3b
CaioAndersonMM <caioandersonmartins@gmail.com>
2025-01-28 03:26:42 -0300

    feat: Implementado funcionalidade para adicionar nódulos dinamicamente, botão e suporte no componente NoduleInfo

--- laudocs/src/components/typeultrassom/NoduleInfo.tsx
+++ laudocs/src/components/typeultrassom/NoduleInfo.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import { noduleQuestions } from '../../utils/question';
-
 
 interface NoduleInfoProps {
     noduleLocation: string;
     formState: any;
-    handleNoduleChange: (side: "esquerda" | "direita", questionLabel: string, value: string) => void;
+    handleNoduleChange: (side: "esquerda" | "direita", questionLabel: string, value: string, noduleIndex: number) => void;
+    handleAddNodule: (side: "esquerda" | "direita") => void;
 }
 
 const NoduleInfo: React.FC<NoduleInfoProps> = ({
     noduleLocation,
     formState,
-    handleNoduleChange
+    handleNoduleChange,
+    handleAddNodule
 }) => {
+    const [noduleCount, setNoduleCount] = useState<{ esquerda: number, direita: number }>({ esquerda: 0, direita: 0 });
+
+    const addNodule = (side: "esquerda" | "direita") => {
+        handleAddNodule(side);
+        setNoduleCount(prev => ({ ...prev, [side]: prev[side] + 1 }));
+    };
 
     return (
         <div>
@@ -24,69 +31,93 @@ const NoduleInfo: React.FC<NoduleInfoProps> = ({
 
             {(noduleLocation === 'Esquerda' || noduleLocation === 'Ambas') && (
                 <>
-                    <h2 className="mt-6 text-lg font-semibold">Informações sobre o nódulo na posição Esquerda</h2>
-                    <div className="grid grid-cols-4 gap-4 mt-4">
-                        {noduleQuestions.map((question, index) => (
-                            <div key={index} className="mb-4 p-4 bg-gray-100 rounded-lg shadow-md hover:bg-gray-300 transition-all duration-300 ease-in-out">
-                                <label className="block text-sm font-semibold text-gray-700">
-                                    {question.label}
-                                    {question.isNumberInput ? (
-                                        <input
-                                            type="number"
-                                            value={formState.noduleData.esquerda[question.label] || ""}
-                                            onChange={(e) => handleNoduleChange("esquerda", question.label, e.target.value)}
-                                            className="mt-2 p-2 border rounded-md w-full bg-white focus:ring-2 focus:ring-cyan-800"
-                                            placeholder="Digite a medida"
-                                        />
-                                    ) : (
-                                        <select
-                                            value={formState.noduleData.esquerda[question.label] || question.options[0]}
-                                            onChange={(e) => handleNoduleChange("esquerda", question.label, e.target.value)}
-                                            className="mt-2 p-2 border rounded-md w-full bg-white focus:ring-2 focus:ring-cyan-800"
-                                        >
-                                            {question.options.map((option, idx) => (
-                                                <option key={idx} value={option}>{option}</option>
-                                            ))}
-                                        </select>
-                                    )}
-                                </label>
+                    <h2 className="mt-6 text-lg font-semibold">Informações sobre o(s) nódulo(s) na posição Esquerda</h2>
+                    {Array.from({ length: noduleCount.esquerda }).map((_, noduleIndex) => (
+                        <div key={noduleIndex} className="mt-6">
+                            <h3 className="text-md font-semibold">Nódulo {noduleIndex + 1}</h3>
+                            <div className="grid grid-cols-4 gap-4 mt-4">
+                                {noduleQuestions.map((question, index) => (
+                                    <div key={index} className="mb-4 p-4 bg-gray-100 rounded-lg shadow-md hover:bg-gray-300 transition-all duration-300 ease-in-out">
+                                        <label className="block text-sm font-semibold text-gray-700">
+                                            {question.label}
+                                            {question.isNumberInput ? (
+                                                <input
+                                                    type="number"
+                                                    value={formState.noduleData.esquerda[noduleIndex]?.[question.label] || ""}
+                                                    onChange={(e) => handleNoduleChange("esquerda", question.label, e.target.value, noduleIndex)}
+                                                    className="mt-2 p-2 border rounded-md w-full bg-white focus:ring-2 focus:ring-cyan-800"
+                                                    placeholder="Digite a medida"
+                                                />
+                                            ) : (
+                                                <select
+                                                    value={formState.noduleData.esquerda[noduleIndex]?.[question.label] || question.options[0]}
+                                                    onChange={(e) => handleNoduleChange("esquerda", question.label, e.target.value, noduleIndex)}
+                                                    className="mt-2 p-2 border rounded-md w-full bg-white focus:ring-2 focus:ring-cyan-800"
+                                                >
+                                                    {question.options.map((option, idx) => (
+                                                        <option key={idx} value={option}>{option}</option>
+                                                    ))}
+                                                </select>
+                                            )}
+                                        </label>
+                                    </div>
+                                ))}
                             </div>
-                        ))}
-                    </div>
+                        </div>
+                    ))}
+                    <button
+                        type='button'
+                        onClick={() => addNodule("esquerda")}
+                        className="mt-4 p-2 bg-cyan-800 text-white rounded-md hover:bg-cyan-900 transition-all duration-300 ease-in-out"
+                    >
+                        Adicionar Nódulo Esquerdo
+                    </button>
                 </>
             )}
 
             {(noduleLocation === 'Direita' || noduleLocation === 'Ambas') && (
                 <>
-                    <h2 className="mt-6 text-lg font-semibold">Informações sobre o nódulo na posição Direita</h2>
-                    <div className="grid grid-cols-4 gap-4 mt-4">
-                        {noduleQuestions.map((question, index) => (
-                            <div key={index} className="mb-4 p-4 bg-gray-100 rounded-lg shadow-md hover:bg-gray-200 transition-all duration-300 ease-in-out">
-                                <label className="block text-sm font-semibold text-gray-700">
-                                    {question.label}
-                                    {question.isNumberInput ? (
-                                        <input
-                                            type="number"
-                                            value={formState.noduleData.direita[question.label] || ""}
-                                            onChange={(e) => handleNoduleChange("direita", question.label, e.target.value)}
-                                            className="mt-2 p-2 border rounded-md w-full bg-white focus:ring-2 focus:ring-cyan-800"
-                                            placeholder="Digite a medida"
-                                        />
-                                    ) : (
-                                        <select
-                                            value={formState.noduleData.direita[question.label] || question.options[0]}
-                                            onChange={(e) => handleNoduleChange("direita", question.label, e.target.value)}
-                                            className="mt-2 p-2 border rounded-md w-full bg-white focus:ring-2 focus:ring-cyan-800"
-                                        >
-                                            {question.options.map((option, idx) => (
-                                                <option key={idx} value={option}>{option}</option>
-                                            ))}
-                                        </select>
-                                    )}
-                                </label>
+                    <h2 className="mt-6 text-lg font-semibold">Informações sobre o(s) nódulo(s) na posição Direita</h2>
+                    {Array.from({ length: noduleCount.direita }).map((_, noduleIndex) => (
+                        <div key={noduleIndex} className="mt-6">
+                            <h3 className="text-md font-semibold">Nódulo {noduleIndex + 1}</h3>
+                            <div className="grid grid-cols-4 gap-4 mt-4">
+                                {noduleQuestions.map((question, index) => (
+                                    <div key={index} className="mb-4 p-4 bg-gray-100 rounded-lg shadow-md hover:bg-gray-200 transition-all duration-300 ease-in-out">
+                                        <label className="block text-sm font-semibold text-gray-700">
+                                            {question.label}
+                                            {question.isNumberInput ? (
+                                                <input
+                                                    type="number"
+                                                    value={formState.noduleData.direita[noduleIndex]?.[question.label] || ""}
+                                                    onChange={(e) => handleNoduleChange("direita", question.label, e.target.value, noduleIndex)}
+                                                    className="mt-2 p-2 border rounded-md w-full bg-white focus:ring-2 focus:ring-cyan-800"
+                                                    placeholder="Digite a medida"
+                                                />
+                                            ) : (
+                                                <select
+                                                    value={formState.noduleData.direita[noduleIndex]?.[question.label] || question.options[0]}
+                                                    onChange={(e) => handleNoduleChange("direita", question.label, e.target.value, noduleIndex)}
+                                                    className="mt-2 p-2 border rounded-md w-full bg-white focus:ring-2 focus:ring-cyan-800"
+                                                >
+                                                    {question.options.map((option, idx) => (
+                                                        <option key={idx} value={option}>{option}</option>
+                                                    ))}
+                                                </select>
+                                            )}
+                                        </label>
+                                    </div>
+                                ))}
                             </div>
-                        ))}
-                    </div>
+                        </div>
+                    ))}
+                    <button
+                        type='button'
+                        onClick={() => addNodule("direita")}
+                        className="mt-4 p-2 bg-cyan-800 text-white rounded-md hover:bg-cyan-900 transition-all duration-300 ease-in-out"
+                    >
+                        Adicionar Nódulo Direito
+                    </button>
                 </>
             )}
         </div>
